980cf41d3e86c01904870c506f7f951caa0941e5
PikarryPham <phamngocthuytrang1026@gmail.com>
2020-05-21 08:46:26 +0700

    fix features, add comments

--- Speech Synthesis/js/index.js
+++ Speech Synthesis/js/index.js
@@ -1,1 +1,67 @@
-const msg=new SpeechSynthesisUtterance;let voices=[];const voicesDropdown=document.querySelector('[name="voice"]'),options=document.querySelectorAll('[type="range"], [name="text"]'),speakButton=document.querySelector("#speak"),stopButton=document.querySelector("#stop");msg.text=document.querySelector('[name="text"]').value;const body=document.querySelector("body");function populateVoices(){voices=this.getVoices(),voicesDropdown.innerHTML=voices.filter(e=>e.lang.includes("en")).map(e=>`<option value="${e.name}">${e.name} (${e.lang})</option>`).join("")}function setVoice(){msg.voice=voices.find(e=>e.name===this.value),toggle()}function toggle(e=!0){speechSynthesis.cancel(),e?(speechSynthesis.speak(msg),body.style.background="url(img/wave.gif)",body.style.backgroundRepeat="repeat-x",body.style.backgroundSize="100% 100%"):body.style.background=""}function setOption(){console.log(this.name,this.value),msg[this.name]=this.value,"rate"==this.name?document.getElementById("voice_range").innerHTML=this.value:"pitch"==this.name&&(document.getElementById("voice_pitch").innerHTML=this.value),toggle()}speechSynthesis.addEventListener("voiceschanged",populateVoices),voicesDropdown.addEventListener("change",setVoice),options.forEach(e=>e.addEventListener("change",setOption)),speakButton.addEventListener("click",toggle),stopButton.addEventListener("click",()=>toggle(!1));
+const msg = new SpeechSynthesisUtterance();
+let voices = []; //mảng voice này chứa những âm thanh mà ta có được từ google, nhưng hiện tại chỉ lấy được 2 voice
+/**
+ * Tham khảo tại: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance/voice
+ */
+
+const voicesDropdown = document.querySelector('[name="voice"]'),
+  options = document.querySelectorAll('[type="range"], [name="text"]'),
+  speakButton = document.querySelector("#speak"),
+  stopButton = document.querySelector("#stop");
+msg.text = document.querySelector('[name="text"]').value; //giá trị của message (để phát ra tiếng nói) sẽ là dữ liệu mà ta ghi ở khung text
+
+/**
+ * Hàm lấy voice
+ */
+const body = document.querySelector("body");
+function populateVoices() {
+  (voices = this.getVoices()),
+    (voicesDropdown.innerHTML = voices
+      .filter(e => e.lang.includes("en"))
+      /**
+       * hàm filter: nhận đối số giống map & hoạt động giống map. callback trả về là true/false
+       * nếu nó là true mảng không thay đổi nếu là false phần tử đó sẽ được lọc ra khỏi mảng ban đầu
+       */
+      .map(e => `<option value="${e.name}">${e.name} (${e.lang})</option>`)
+      //dùng hàm map để khi ta muốn thay đổi/thêm các thuộc tính của từng đối tượng cho các object của mảng voice
+      /**
+       * Ví dụ tương ứng với name nào sẽ tương ứng với voice của name đó ==> đưa giá trị vào trong value của option của select bên file html
+       * Hiện tại, k thể lấy nhiều hơn 2 web speech API cho voice ==> chưa sửa được vấn đề này
+       */
+      .join(""));
+}
+/**
+ * Hàm set giọng nói tương ứng với option mà người dùng ch
+ */
+function setVoice() {
+  (msg.voice = voices.find(e => e.name === this.value)), toggle();
+}
+function toggle(e = !0) {
+  //e != 0 tương đương e == true (e ở đây là event)
+  //nếu ta bấm nút speak thì ==> nói + gif wave cho background xuất hiện
+  speechSynthesis.cancel(),
+    e // e?true ==> hiển thị background:false==>không hiển thị background
+      ? (speechSynthesis.speak(msg),
+        (body.style.background = "url(img/wave.gif)"),
+        (body.style.backgroundRepeat = "repeat-x"),
+        (body.style.backgroundSize = "100% 100%"))
+      : (body.style.background = "");
+}
+function setOption() {
+  //hàm set option cho pitch (tông nói) và rate(tốc độ nói)
+  console.log(this.name, this.value),
+    (msg[this.name] = this.value),
+    "rate" == this.name
+      ? (document.getElementById("voice_range").innerHTML = this.value)
+      : "pitch" == this.name &&
+        (document.getElementById("voice_pitch").innerHTML = this.value),
+    toggle();
+}
+/**
+ * Thêm sự kiện vào cho các object
+ */
+speechSynthesis.addEventListener("voiceschanged", populateVoices),
+  voicesDropdown.addEventListener("change", setVoice),
+  options.forEach(e => e.addEventListener("change", setOption)),
+  speakButton.addEventListener("click", toggle),
+  stopButton.addEventListener("click", () => toggle(!1));
--- Speech Synthesis/js/index2.js
+++ Speech Synthesis/js/index2.js
@@ -2,12 +2,12 @@ const searchForm = document.querySelector("#search-form");
 const searchFormInput = searchForm.querySelector("input"); // <=> document.querySelector("#search-form input") (có thể dùng 2 cách)
 const info = document.querySelector(".info");
 
-// The speech recognition interface lives on the browser’s window object
+//Giao diện speech recognition nằm ở object toàn cục của trình duyệt windows
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition; // nếu không phát hiện ra giọng nói ==> gán undefined
 
 if (SpeechRecognition) {
-  console.log("Your Browser supports speech Recognition");
+  console.log("Not Supported");
 
   const recognition = new SpeechRecognition();
   recognition.continuous = true;
@@ -16,6 +16,7 @@ if (SpeechRecognition) {
     "beforeend",
     '<button type="button"><i class="fas fa-microphone"></i></button>'
   );
+  //chèn icon microphone vào
   searchFormInput.style.paddingRight = "50px";
 
   const micBtn = searchForm.querySelector("button");
@@ -24,7 +25,7 @@ if (SpeechRecognition) {
   micBtn.addEventListener("click", micBtnClick);
   function micBtnClick() {
     if (micIcon.classList.contains("fa-microphone")) {
-      // Start Voice Recognition
+      // Bắt đầu Voice Recognition
       recognition.start(); // Lần đầu tiên khi kết nối tới mic
     } else {
       recognition.stop();
@@ -36,18 +37,18 @@ if (SpeechRecognition) {
     micIcon.classList.remove("fa-microphone");
     micIcon.classList.add("fa-microphone-slash");
     searchFormInput.focus();
-    console.log("Voice activated, SPEAK");
+    console.log("SPEAK!!!");
   }
 
   recognition.addEventListener("end", endSpeechRecognition); // <=> recognition.onend = function() {...}(có 2 cách ghi)
   function endSpeechRecognition() {
-    micIcon.classList.remove("fa-microphone-slash");
+    micIcon.classList.remove("fa-microphone-slash"); //ngưng nói ==> trở lại icon microphone ban đầu
     micIcon.classList.add("fa-microphone");
     searchFormInput.focus();
     console.log("Speech recognition service disconnected");
   }
 
-  recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
+  recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...}
   function resultOfSpeechRecognition(event) {
     const current = event.resultIndex;
     const transcript = event.results[current][0].transcript;
@@ -60,8 +61,12 @@ if (SpeechRecognition) {
     } else {
       if (transcript.toLowerCase().trim() === "go") {
         //dùng lệnh này để bắt đầu search
-        searchForm.submit();
-      } else if (transcript.toLowerCase().trim() === "clear") {
+        /**
+         * phương thức trim(): Remove whitespace ở cả hai phía của chuỗi
+         * ví dụ: var str = "       Hello World!        "; ==> console.log(str) ==> Hello World
+         */
+        searchForm.submit(); //khi submit, form bên file index2.html sẽ: dùng phương thức "get" (method = get), gọi đến hành động truy cập vào đường link của google "action = https://www.google.com/search")
+      } else if (transcript.toLowerCase().trim() === "delete") {
         //dùng lệnh này để clear input và bắt đầu record lại
         searchFormInput.value = "";
       } else {
@@ -70,7 +75,7 @@ if (SpeechRecognition) {
     }
   }
 
-  info.textContent = 'Voice Commands: "stop recording", "clear", "go"';
+  info.textContent = 'Voice Commands: "stop recording", "delete", "go"';
 } else {
   console.log(
     "Trình duyệt của bạn không hỗ trợ Speech Recognition/Oops...Your browser doesn't support Speech Recognition"
